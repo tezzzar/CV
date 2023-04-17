dae55a5b7baf8730f45f43fff20f00e68aebd9e3
tezzzar <olexandr.borodavko@gmail.com>
2023-04-17 17:21:48 +0300

    add tabs

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
   $(".slider-main").slick({
     arrows: false,
     adaptiveHeight: true,
-    autoplay: true,
+    autoplay: false,
     dots: true,
     speed: 1000,
     autoplaySpeed: 10000,
@@ -25,10 +25,32 @@ $(document).ready(function () {
 let toggle = document.querySelector(".toggle");
 let about = document.querySelector(".about");
 let arrow = document.querySelector(".arrow");
-toggle.addEventListener("click", function () {
-  about.classList.toggle("show");
-  arrow.classList.toggle("arrow-down");
+let slider = document.querySelector(".block-shadow");
+let portfolio = document.querySelector(".portfolio");
+let tab1 = document.getElementById("tab-1");
+let tab2 = document.getElementById("tab-2");
+let tab3 = document.getElementById("tab-3");
+tab1.addEventListener("click", function () {
+  tab1.classList.add("tab-bg", "font-bold");
+  tab2.classList.remove("tab-bg", "font-bold");
+  tab3.classList.remove("tab-bg", "font-bold");
+  slider.classList.remove("hide");
+  about.classList.remove("show");
+  portfolio.classList.add("hide");
 });
-
-console.log(toggle);
-console.log(about);
+tab2.addEventListener("click", function () {
+  tab1.classList.remove("tab-bg", "font-bold");
+  tab2.classList.add("tab-bg", "font-bold");
+  tab3.classList.remove("tab-bg", "font-bold");
+  slider.classList.add("hide");
+  about.classList.add("show");
+  portfolio.classList.add("hide");
+});
+tab3.addEventListener("click", function () {
+  tab1.classList.remove("tab-bg", "font-bold");
+  tab2.classList.remove("tab-bg", "font-bold");
+  tab3.classList.add("tab-bg", "font-bold");
+  slider.classList.add("hide");
+  about.classList.remove("show");
+  portfolio.classList.remove("hide");
+});
